9f0d3f4bfc64e7d1cf97dde24ee9ec9a761a4c21
lauMv <42174201+lauMv@users.noreply.github.com>
2023-02-04 00:18:03 -0400

    setting DB

---
+++ backend/db_schemas/user_schema.py
@@ -0,0 +1,20 @@
+from marshmallow import Schema, fields, EXCLUDE
+
+
+class UserSchema(Schema):
+    first_name = fields.Str(allow_none=False)
+    last_name = fields.Str(allow_none=False)
+    type = fields.Str(allow_none=False)
+    birthday = fields.Date(allow_none=False)
+    document_id = fields.Integer(allow_none=False)
+    country = fields.Str(allow_none=False)
+    city = fields.Str(allow_none=False)
+    address = fields.Str(allow_none=True)
+    email = fields.Str(allow_none=False)
+    password = fields.Str(allow_none=False)
+    phone_number = fields.Int(allow_none=True)
+    username = fields.Str(allow_none=False)
+    id = fields.Int(allow_none=False)
+
+    class Meta:
+        unknown = EXCLUDE
