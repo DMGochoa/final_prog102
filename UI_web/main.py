c08bc529a6ec6936b42214fa72e894ab492839ea
DMGochoa <diego.moreno@utp.edu.co>
2023-02-04 20:28:45 -0500

    Some changes in the date selector, new buttom and a description

--- UI_web/main.py
+++ UI_web/main.py
@@ -7,13 +7,15 @@ import dash_bootstrap_components as dbc
 # Pages
 from pages.register import register_form
 
-app = Dash(__name__,
-           external_stylesheets=[
-               dbc.themes.LUX, dbc.icons.BOOTSTRAP], suppress_callback_exceptions=True,
+app = Dash(__name__, 
+           external_stylesheets=[dbc.themes.LUX, 
+                                 dbc.icons.BOOTSTRAP], 
+           suppress_callback_exceptions=True,
            meta_tags=[{"name": "viewport",
                        "content": "width=device-width, initial-scale=1"}]
            )
 
+app.title = "Bank"
 
 df = pd.DataFrame({
     "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
@@ -25,17 +27,9 @@ fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
-
-    #html.Div(children='''
-    #    Dash: A web application framework for your data.
-    #'''),
-
-    #dcc.Graph(
-    #    id='example-graph',
-    #    figure=fig
-    #),
-
-    register_form()
+    dbc.Col(className="col-2",),
+    dbc.Col(register_form() ,className="col-8",),
+    dbc.Col(className="col-2",),   
 ])
 
 if __name__ == '__main__':
--- UI_web/pages/register.py
+++ UI_web/pages/register.py
@@ -1,5 +1,7 @@
 import dash_bootstrap_components as dbc
 from dash import html
+from dash import dcc
+from datetime import date
 
 def form_field(title:str, extra_info:str, space:int):
 
@@ -7,10 +9,10 @@ def form_field(title:str, extra_info:str, space:int):
         [
             dbc.Label(title, html_for=title.lower() + '_field'),
             dbc.Input(type=title.lower(), id=title.lower() + '_field', placeholder="Enter " + title),
-            dbc.FormText(
-                    extra_info,
-                    color="secondary",
-                ),
+            #dbc.FormText(
+            #        extra_info,
+            #       color="secondary",
+            #    ),
         ],
         className=f"mb-{space}",
     )
@@ -39,24 +41,40 @@ extra_data = ['Please enter the ' +  titles[0],
 
 
 def register_form():
+
+    # Selectors
     f_name = form_field(titles[0], extra_data[0], 6)
     l_name = form_field(titles[1], extra_data[1], 3)
     nationid = form_field(titles[2], extra_data[2], 6)
-    bdate = form_field(titles[3], extra_data[3], 3)
+    #bdate = form_field(titles[3], extra_data[3], 3)
     country = form_field(titles[4], extra_data[4], 6)
     city = form_field(titles[5], extra_data[5], 3)
     address = form_field(titles[6], extra_data[6], 6)
     email = form_field(titles[7], extra_data[7], 3)
     cellpho = form_field(titles[8], extra_data[8], 6)
+
+    bdate = dcc.DatePickerSingle(
+        month_format='DD/MM/YYYY',
+        placeholder='DD/MM/YYYY',
+        display_format='DD/MM/YYYY',
+        clearable=True,
+        date=date.today(),
+        style={"border-top": "2px"}
+    )
+
     scheme = html.Div(
         [
+            dbc.Row([html.H3('Please enter the following information to create a new user.')]),
             dbc.Row([
                 dbc.Col(f_name), 
                 dbc.Col(l_name),
             ]),
             dbc.Row([
                 dbc.Col(nationid), 
-                dbc.Col(bdate),
+                dbc.Col([
+                    html.P('something'),
+                    bdate
+                ]),
             ]),
             dbc.Row([
                 dbc.Col(country), 
@@ -68,8 +86,11 @@ def register_form():
             ]),
             dbc.Row([
                 dbc.Col([email])
-            ])
-        ], className="col-12 col-lg-6"
+            ]),
+            dbc.Row(html.Center(html.P(
+                dbc.Button("SUBMIT", color="primary"), className="create_user"
+            )))
+        ], className="col-12 col-lg-6 ",
     )
     form = dbc.Form([scheme])
     return form
