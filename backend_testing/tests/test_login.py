6da2d5b966ee3e90eba636994ad70533d94e7ed0
Camiloarceg <camiloarceg@gmail.com>
2023-02-27 13:57:07 -0500

    extra negative test wrong pass and code

--- backend_testing/tests/test_login.py
+++ backend_testing/tests/test_login.py
@@ -23,3 +23,47 @@ def test_login(client):
     }
     response = client.post(f"{LOGIN_ENDPOINT}",json=login_json)
     assert response.status_code == 200
+
+def test_wrong_password(client):
+    user_json = {
+            "first_name": "jhon",
+            "last_name": "doe",
+            "type": "user",
+            "document_id": "12345678",
+            "birthday": "1997-01-01",
+            "country": "peru",
+            "city": "lima",
+            "address": "av siempreviva",
+            "email": "ca@texample.com",
+            "phone_number": "999555999"
+    }
+    user_response = client.post(f"{USERS_ENDPOINT}",json=user_json)
+    login_json = {
+        "username": user_response.json["username"],
+        "password": "anypass123",
+        "code": int(user_response.json["code"])
+    }
+    response = client.post(f"{LOGIN_ENDPOINT}",json=login_json)
+    assert response.status_code == 400
+
+def test_wrong_code(client):
+    user_json = {
+            "first_name": "jhon",
+            "last_name": "doe",
+            "type": "user",
+            "document_id": "12345678",
+            "birthday": "1997-01-01",
+            "country": "peru",
+            "city": "lima",
+            "address": "av siempreviva",
+            "email": "ca@texample.com",
+            "phone_number": "999555999"
+    }
+    user_response = client.post(f"{USERS_ENDPOINT}",json=user_json)
+    login_json = {
+        "username": user_response.json["username"],
+        "password": user_response.json["password"],
+        "code": 123456789
+    }
+    response = client.post(f"{LOGIN_ENDPOINT}",json=login_json)
+    assert response.status_code == 400
